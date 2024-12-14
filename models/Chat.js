d2f6364e0e8edc057c24c5d076fdf3077a26edc8
9ian9 <108910700+9ian9@users.noreply.github.com>
2024-12-15 02:02:49 +0700

    done chat and frontend translate(need to adjust for easy viewing )

---
+++ models/Chat.js
@@ -0,0 +1,29 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('mysql://root@localhost:3306/pbl6', {
+    dialect: 'mysql',
+    logging: console.log
+});
+
+const Chat = sequelize.define('Chat', {
+    sender_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    receiver_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    message: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    timestamp: {
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.NOW,
+    },
+}, {
+    tableName: 'chat',
+    timestamps: false,
+});
+
+module.exports = Chat;
--- routes/videoRouter.js
+++ routes/videoRouter.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const videoController = require('../controllers/videoController');
 
-router.get('/call', videoController.getCallPage);
+// router.get('/call', videoController.getCallPage);
 router.post('/start-call', videoController.startCall);
 
 // Route để hiển thị trang video.ejs
-router.get('/video', videoController.getVideoPage);
+// router.get('/video', videoController.getVideoPage);
 
 
 module.exports = router;
